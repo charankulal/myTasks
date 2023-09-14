25f3066ca737e731dc4e3a0a569936ae525233c6
Charan <charankulal0241@gmail.com>
2023-09-14 21:43:25 +0530

    Drak mode is applied for task modal

--- src/Components/NewTaskModal.js
+++ src/Components/NewTaskModal.js
@@ -1,37 +1,79 @@
-import React from 'react'
+import React from "react";
+import "../Styles/NewTaskModal.css"
 
 export default function NewTaskModal(props) {
   return (
     <>
-    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Add a Task </button>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Add a Task{" "}
+      </button>
 
-<div  className={`row modal fade ${props.isDarkMode ? 'dark-mode' : 'light-mode'}`} id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">Add a Task</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        <form>
-          <div className="mb-3">
-            <label for="recipient-name" className="col-form-label">Title</label>
-            <input type="text" className="form-control" id="title" placeholder='Eg : Study for the exam'/>
-          </div>
-          <div className="mb-3">
-            <label for="message-text" className="col-form-label">Message:</label>
-            <textarea className="form-control" id="message-text"></textarea>
-          </div>
-        </form>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      <div
+        className={`modal fade ${
+          props.isDarkMode ? "dark-mode" : "light-mode"
+        }`}
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        data-bs-theme={`${
+            props.isDarkMode ? "dark" : "light"
+          }`}
         
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Add a Task
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label for="recipient-name" className="col-form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    placeholder="Eg : Study for the exam"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="message-text" className="col-form-label">
+                    Message:
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="message-text"
+                  ></textarea>
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
-      
     </>
-  )
+  );
 }

616321fe27b1b34ce4a79e964ecad613888281e8
Charan <charankulal0241@gmail.com>
2023-10-05 22:34:59 +0530

    Added toggle functionality for icons

--- src/Components/Directory.js
+++ src/Components/Directory.js
@@ -1,42 +1,41 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import "../Styles/Navbar.css";
-import $ from 'jquery'
+import $ from "jquery";
 import AddNewDitectoryModal from "./AddNewDirectoryModal";
 
 export default function Directory(props) {
+  $(document).ready(function () {
+    let i=1;
+    $(".directory").on('click',function () {
+      i=i+1
+      if (i%2===1) {
+        $("i").removeClass("bi-chevron-right")
+        $('i').addClass('bi-chevron-down')
+        document.getElementById('main').style.display='block'
 
-$(document).ready(function () {
-  $(".directory").click(function () {
-    // var isActive = $(".bi").hasClass('bi-chevron-right');
-    if ($("i").hasClass('bi-chevron-right')){
-
-      $(".bi-chevron-right").toggleClass("bi-chevron-right bi-chevron-down");
-
-     
-    }
-   
-   
+      }
+      else if (i%2===0) {
+        $("i").removeClass("bi-chevron-down")
+        $('i').addClass('bi-chevron-right')
+        document.getElementById('main').style.display='none'
+       
+        
+      }
+      
+      
+    });
   });
-});
-$(".directory").click(function(){
-  if ($("i").hasClass('bi-chevron-down')){
-
-  // console.log("Clicked")
-  $(".bi-chevron-down").toggleClass("bi-chevron-down bi-chevron-right");
-  }
-})
 
   return (
     <>
- 
       <div className="justify-content-center  m-3">
-        <div className=".fs-4 directory " >
+        <div className=".fs-4 directory ">
           Directories
           <i className="bi bi-chevron-right"></i>
         </div>
 
-        <p className="mx-3">Main</p>
+        <p className="mx-3 main" id="main" style={{display:'none'}}>Main</p>
 
         <AddNewDitectoryModal isDarkMode={props.isDarkMode} />
       </div>

a4ceba625ff444ef7f8c478b9a0d4c8030b0a8a8
Charan <charankulal0241@gmail.com>
2023-11-05 18:08:09 +0530

    Fixed bugs

--- src/Components/CenterSection.js
+++ src/Components/CenterSection.js
@@ -67,7 +67,7 @@ export default function CenterSection(props) {
     description: "",
     date: "",
     dir: "Main",
-    completed: true,
+    completed: false,
     id: "",
   });
   const [task1, setTask1] = useState({
@@ -326,7 +326,7 @@ export default function CenterSection(props) {
               <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
             </svg>
           </div>
-          <div className="col-3 align-self-end">
+          {/* <div className="col-3 align-self-end">
             <select
               className="form-select sortlist"
               aria-label="Default select example"
@@ -339,7 +339,7 @@ export default function CenterSection(props) {
               <option value="4">Completed First</option>
               <option value="5">Uncompleted First</option>
             </select>
-          </div>
+          </div> */}
         </div>
         <button
           type="button"
